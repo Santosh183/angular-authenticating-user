cd89a9aa5a77d77f7e59cd83c31ef9adef3686b4
Santosh Kadam <skadam183@gmail.com>
2019-05-27 09:39:44 +0530

    changes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,10 +8,10 @@ export class UserService {
     constructor(private http: HttpClient) { }
 
     getAll() {
-        return this.http.get<User[]>('http://localhost:4000/users');
+        return this.http.get<User[]>(`${config.apiUrl}/users`);
     }
 
     getById(id: number) {
-        return this.http.get<User>(`http://localhost:4000/users/${id}`);
+        return this.http.get<User>(`${config.apiUrl}/users/${id}`);
     }
 }
